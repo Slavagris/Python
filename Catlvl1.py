7d37de6f8c40a55a0297a41fc581ae66682563fa
Slava Grishin <83882153+Slavagris@users.noreply.github.com>
2021-12-04 23:00:00 +0300

    Create Catlvl1.py

---
+++ Catlvl1.py
@@ -0,0 +1,28 @@
+# The Class Cat 
+# Author Slava Grishin
+# Version 04.12.2021
+# There are 3 classes of Python language used here: eat,cat,dish
+class Cat:
+    def __init__(self,name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+    Cat = print("Name:Baks", "age:5", "breed:mixed");
+class Dish:
+    def __init__(self,width,height,volume, filled):
+        self.width = width
+        self.height = height
+        self.volume = volume
+        self.filled = filled
+    Dish = print("Width = 20", "Height= 5","volume = 10");
+    filled =1000
+    if (filled < 100):print("Миска пустая" , "Кот останется голодный")
+    if (filled > 100):print("Миска будет переполнена")
+    if (filled == 100):print("Миска будет заполнена")
+class eat:
+    def __init__(self,eat):
+        self.eat = eat
+    eat=print(0+ Dish.filled);
+   
+
+       
