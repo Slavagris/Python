7aa1b0f8158c7655876d109ac93d00f3ed648ca1
Slava Grishin <83882153+Slavagris@users.noreply.github.com>
2021-12-09 16:43:05 +0300

    Create ListAnimals.py

---
+++ ListAnimals.py
@@ -0,0 +1,80 @@
+# The Class ListAnimals
+# Author Slava Grishin 
+# version 09.12.2021
+class Humans:
+    def use_ListAnimals(self):
+        pass
+class Human(Humans):
+    def use_ListAnimals(self):
+        print("Люди Использует список животных");
+Yulia = Human()
+Yulia.use_ListAnimals()
+class ListAnimals:
+    def __init__ (self, name, age, view):
+        self.name = name
+        self.age = age
+        self.view = view
+    ListAnimals = print("name", "age", "view");
+    ListAnimals = print("name = Baks", "age = 5", "view = cat");
+    ListAnimals = print("name = Jack", "age = 6", "view = Dog");
+    ListAnimals = print("name = Andrey", "age = 8", "view = horse");
+    ListAnimals = print("name = Rita", "age = 9", "view = cow");
+    ListAnimals = print("name = Masha", "age = 7", "view = pig");
+    ListAnimals = print("name = Yana", "age = 8", "view = chiсken");
+    ListAnimals = print("name = Vlad", "age = 2", "view = fish");
+    
+    
+    
+    
+class Animals:
+    def eat(self):
+        pass
+class Cat(Animals):
+    def voice_voice(self):
+        print("Мяу");
+
+Baks = Cat()
+Baks.voice_voice()
+
+class Dog(Animals):
+    def voice_voice(self):
+        print("Гав Гав");
+
+Jack = Dog()
+Jack.voice_voice()
+
+class horse(Animals):
+    def voice_voice(self):
+        print("Храп");
+Andrey = horse()
+Andrey.voice_voice()
+
+class cow(Animals):
+    def voice_voice(self):
+        print("Му Му");
+Rita = cow()
+Rita.voice_voice()
+
+class pig(Animals):
+    def voice_voice(self):
+        print("a");
+Masha = pig()
+Masha.voice_voice()
+
+class chiсken(Animals):
+    def voice_voice(self):
+        print("Ко-Ко-Ко");
+Yana = chiсken()
+Yana.voice_voice()
+
+class fish(Animals):
+    def swing():
+        pass
+Vlad = fish()
+fish.swing()
+# Массив жэивотных 
+Animals = ["Cat", "Pig", "chiсken", "Dog", "Horse", "Cow", "fish"]
+Animals.sort
+print(Animals)
+Human = ["Yulia"]
+print(Human)
